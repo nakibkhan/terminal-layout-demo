b5f7599b3da72a2b85fe1a1f594a77fad1f443d7
nakibkhan <nakib.khan1988@gmail.com>
2019-08-31 23:39:44 -0500

    initial bootstrap commit

---
+++ src/main/groovy/com/example/terminallayoutdemo/TerminalLayoutDemoApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.terminallayoutdemo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TerminalLayoutDemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TerminalLayoutDemoApplication, args)
+	}
+
+}
